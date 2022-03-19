40951052fceba8cd2830e520c37f5cc6d20cfb2c
Thea-Vi <tei.villanueva@gmail.com>
2022-03-18 21:26:44 -0700

    Practice Faker API

---
+++ server.js
@@ -0,0 +1,48 @@
+const { faker } = require("@faker-js/faker");
+const express = require("express");
+const app = express();
+const port = 8000;
+
+class User {
+  constructor() {
+    this._userid = faker.datatype.uuid();
+    this.firstName = faker.name.firstName();
+    this.lastName = faker.name.lastName();
+    this.phoneNumber = faker.phone.phoneNumber();
+    this.email = faker.internet.email();
+    this.password = faker.internet.password();
+  }
+}
+
+class Company {
+  constructor() {
+    this._companyid = faker.datatype.uuid();
+    this.name = faker.company.companyName();
+    this.address = {
+      street: faker.address.streetName(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode(),
+      country: faker.address.country(),
+    };
+  }
+}
+
+app.get("/api/users/new", (req, res) => {
+  res.json(new User());
+  console.log(new User());
+});
+app.get("/api/companies/new", (req, res) => {
+  res.json(new Company());
+
+  console.log(new Company());
+});
+app.get("/api/user/company", (req, res) => {
+  res.json({
+    user: new User(),
+    company: new Company(),
+  });
+});
+
+// this needs to below the other code blocks
+app.listen(port, () => console.log(`Listening on port: ${port}`));
